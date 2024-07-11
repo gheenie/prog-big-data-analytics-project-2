6d94a589c04a0c0e39b8dcc28a698e9b43bc553b
infoomics <sbx_94@hotmail.com>
2024-07-11 23:37:18 +0100

    Complete query 2

--- task_5.py
+++ task_5.py
@@ -5,7 +5,7 @@ from config._env_development import (user, password)
 connection = pg8000.native.Connection(user=user, password=password, database='bda_proj_2')
 
 
-def task_4_1(plan_name):
+def query_1(plan_name):
     query = f'''
         SELECT users.*, plan_name
         FROM users
@@ -17,5 +17,18 @@ def task_4_1(plan_name):
         print(row)
 
 
-task_4_1('HD')
-task_4_1('UHD')
+def query_2():
+    query = f'''
+        SELECT *
+        FROM actors
+        LEFT JOIN actors_shows USING (actor_id)
+        LEFT JOIN shows USING (show_id);
+    '''
+    rows = connection.run(query)
+    for row in rows:
+        print(row)
+
+
+query_1('HD')
+query_1('UHD')
+query_2()
